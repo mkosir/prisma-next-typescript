f29980ddb791471bc91987998192ab99b8e7e4a1
mkosir <kosir.mar@gmail.com>
2022-06-30 15:04:37 +0200

    chore: update scripts

--- scripts/db-init.sh
+++ scripts/db-init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+npm config set update-notifier false
 cd app
+
 echo "Install bash and execute 'wait-for-it.sh' script"
 apk add --update bash
 ./scripts/wait-for-it.sh $PG_HOST:5432 --timeout=30 --strict -- echo "postgres up and running"
