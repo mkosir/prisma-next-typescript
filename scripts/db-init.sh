433d3cbcf3097afe93d3dbcf9b522bc0ebac5e45
mkosir <kosir.mar@gmail.com>
2022-06-30 13:40:09 +0200

    chore: update scripts

--- scripts/db-init.sh
+++ scripts/db-init.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Install bash and execute 'wait-for-it.sh' script"
+apk add --update bash
+./scripts/wait-for-it.sh $PG_HOST:5432 --timeout=30 --strict -- echo "postgres up and running"
+
 echo "### Database initialization - Start ###"
 echo "Running migrartions..."
 npm run db-migration-run
