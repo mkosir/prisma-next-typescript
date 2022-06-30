5241fd9adbd3d92cb9821215a2480fb84b86ed5c
mkosir <kosir.mar@gmail.com>
2022-06-30 09:45:34 +0200

    chore: update prisma

---
+++ prisma/migrations/20220630074146_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('APPRENTICE', 'SUPERVISOR', 'ADMINISTRATOR');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT,
+    "name" TEXT,
+    "imageUrl" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'APPRENTICE',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Batch" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "purity" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "Batch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_BatchToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_BatchToUser_AB_unique" ON "_BatchToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_BatchToUser_B_index" ON "_BatchToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_BatchToUser" ADD CONSTRAINT "_BatchToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Batch"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BatchToUser" ADD CONSTRAINT "_BatchToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
