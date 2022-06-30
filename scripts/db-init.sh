ff4f052f821f01ca64ade8a8f98273f779ec505f
mkosir <kosir.mar@gmail.com>
2022-06-30 12:02:25 +0200

    chore: update scripts

--- scripts/db-init.sh
+++ scripts/db-init.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-echo "Install npm"
-apk add --update node
-
 echo "### Database initialization - Start ###"
 echo "Running migrartions..."
 npm run db-migration-run
 echo "Running seeds..."
-npm run db-seed-run
+# npm run db-seed-run
 # npm run db-view
 echo "### Database initialization - End ###"
