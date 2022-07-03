f08c5431f47b0130209fbf0d805f79d41ac50e75
mkosir <kosir.mar@gmail.com>
2022-07-03 21:16:14 +0200

    chore: update prisma

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,11 +1,11 @@
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { batches } from './batches';
 import { users } from './users';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.createMany<{ data: User[] }>({ data: users });
+  await prisma.user.createMany<{ data: Prisma.UserCreateInput[] }>({ data: users });
 
   // await prisma.batch.createMany({
   //   data: batches,
