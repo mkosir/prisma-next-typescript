17ad38e3d3d89756b5ac8317c03adc1e168104a1
mkosir <kosir.mar@gmail.com>
2022-07-03 21:27:54 +0200

    chore: update prisma

--- prisma/seed/users.ts
+++ prisma/seed/users.ts
@@ -3,9 +3,23 @@ import { Prisma } from '@prisma/client';
 export const users: Prisma.UserCreateInput[] = [
   {
     id: '8f008731-4645-43de-8af9-3060d4086001',
-    email: 'walter.white@admin.com',
+    email: 'walter.white@mail.com',
     name: 'Walter White',
     username: 'Heisenberg',
     role: 'ADMINISTRATOR',
+  },
+  {
+    id: '8f008731-4645-43de-8af9-3060d4086002',
+    email: 'jesse.pinkman@mail.com',
+    name: 'Jesse Pinkman',
+    username: 'Jesse',
+    role: 'APPRENTICE',
+  },
+  {
+    id: '8f008731-4645-43de-8af9-3060d4086003',
+    email: 'skyler.white@mail.com',
+    name: 'Skyler White',
+    username: 'Sky',
+    role: 'STANDARD',
   },
 ];
