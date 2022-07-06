4c1ded7a639057e93e554224e9f001ec40fafc94
mkosir <kosir.mar@gmail.com>
2022-07-06 14:25:35 +0200

    chore: update prisma

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,16 +1,16 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { batches } from './batches';
 import { users } from './users';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.createMany<{ data: Prisma.UserCreateInput[] }>({ data: users });
+  await prisma.user.createMany({ data: users });
 
   // Nested relations not supported using createMany -  https://github.com/prisma/prisma/issues/5455
   // await prisma.batch.createMany<{ data: Prisma.BatchCreateInput[] }>({ data: batches });
   for (let batch of batches) {
-    await prisma.batch.create<{ data: Prisma.BatchCreateInput }>({ data: batch });
+    await prisma.batch.create({ data: batch });
   }
 }
 
