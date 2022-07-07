e7ac9a2d601674bc1d02de7a9158065f822d5616
mkosir <kosir.mar@gmail.com>
2022-07-07 14:49:14 +0200

    chore: update api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from 'next';
+import type { InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 
+import prisma from '../prisma/prismaClient';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const Home = ({ users }: ServerSideProps) => {
+  console.log('🔎 Log ~ users', users);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -32,36 +34,24 @@ const Home: NextPage = () => {
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
 
 export default Home;
+
+type ServerSideProps = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export const getServerSideProps = async () => {
+  const users = await prisma.user.findMany({
+    include: {
+      batches: true,
+    },
+  });
+
+  return {
+    props: { users: JSON.parse(JSON.stringify(users)) as typeof users },
+  };
+};
