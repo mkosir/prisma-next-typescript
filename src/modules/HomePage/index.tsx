4ff3465bf5bca28ac6a2917c12f26222b180b9f7
mkosir <kosir.mar@gmail.com>
2022-07-08 17:07:24 +0200

    chore: update api

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,33 +1,12 @@
-import styles from './index.module.css';
-
 export const HomePage = () => {
   return (
     <div>
+      <h3>💊 Minimal GraphQL boilerplate with Prisma, Next.js and TypeScript</h3>
+      <span>Models:</span>
       <ul>
         <li>Users</li>
         <li>Batches</li>
       </ul>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-        </div>
-      </main>
     </div>
   );
 };
