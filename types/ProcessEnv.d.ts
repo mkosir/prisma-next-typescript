4b4c027690b15dc1eb230bc3f3239e1c2e4547f9
mkosir <kosir.mar@gmail.com>
2022-07-08 14:03:19 +0200

    chore: update types

---
+++ types/ProcessEnv.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    PG_HOST: string;
+    PG_PORT: 5432;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DB: string;
+    DATABASE_URL: string;
+  }
+}
