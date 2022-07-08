dca225fe0588b4c1fa98b84c51353b4e92e5460c
mkosir <kosir.mar@gmail.com>
2022-07-08 17:38:37 +0200

    chore: update api

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,11 +1,21 @@
+import Link from 'next/link';
+
 export const HomePage = () => {
   return (
     <div>
       <h3>💊 Minimal GraphQL boilerplate with Prisma, Next.js and TypeScript</h3>
       <span>Models:</span>
       <ul>
-        <li>Users</li>
-        <li>Batches</li>
+        <Link href="/users">
+          <a>
+            <li>Users</li>
+          </a>
+        </Link>
+        <Link href="/batches">
+          <a>
+            <li>Batches</li>
+          </a>
+        </Link>
       </ul>
     </div>
   );
