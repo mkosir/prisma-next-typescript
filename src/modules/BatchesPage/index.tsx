d7a0c5dccb5b952f96143c18000173e3f787a736
mkosir <kosir.mar@gmail.com>
2022-07-11 09:59:02 +0200

    chore: update api

--- src/modules/BatchesPage/index.tsx
+++ src/modules/BatchesPage/index.tsx
@@ -3,5 +3,12 @@ import { ServerSidePropsBatches } from 'pages/batches';
 export const BatchesPage = ({ batches }: ServerSidePropsBatches) => {
   console.log('🔎 Log ~ batches', batches);
 
-  return <div>Batches</div>;
+  return (
+    <div>
+      <h3>💊 Batches</h3>
+      {batches.map((batch) => (
+        <div key={batch.id}>{batch.title}</div>
+      ))}
+    </div>
+  );
 };
--- src/modules/UsersPage/index.tsx
+++ src/modules/UsersPage/index.tsx
@@ -3,5 +3,12 @@ import { ServerSidePropsUsers } from 'pages/users';
 export const UsersPage = ({ users }: ServerSidePropsUsers) => {
   console.log('🔎 Log ~ users', users);
 
-  return <div>Users</div>;
+  return (
+    <div>
+      <h3>👤 Users</h3>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
