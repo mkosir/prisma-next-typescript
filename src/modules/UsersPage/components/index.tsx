953bcb6d3a1a03978f36f5b3513608680aade9ab
mkosir <kosir.mar@gmail.com>
2022-07-11 16:02:39 +0200

    chore: update api

---
+++ src/modules/UsersPage/components/index.tsx
@@ -0,0 +1,29 @@
+import { Prisma } from '@prisma/client';
+
+import { ServerSidePropsUsers } from 'pages/users';
+
+type Users = ServerSidePropsUsers['users'];
+type User = Users[number];
+
+export type UserRowProps = {
+  user: User;
+};
+
+export const UserRow = ({ user }: UserRowProps) => {
+  console.log('🔎 Log ~ user', user);
+
+  const purities = user.batches.map((batch) => batch.purity);
+  const purityBest = purities.length ? Prisma.Decimal.max(...purities).toNumber() : '-';
+  const purityAverage = purities.length ? Prisma.Decimal.sum(...purities).toNumber() : '-';
+
+  return (
+    <div>
+      {user.name} | {user.username} | {user.role} | Stats:
+      <ul>
+        <li>Cooked {user.batches.length} batches</li>
+        <li>Best purity {purityBest}</li>
+        <li>Average purity {purityAverage}</li>
+      </ul>
+    </div>
+  );
+};
--- src/modules/UsersPage/index.tsx
+++ src/modules/UsersPage/index.tsx
@@ -1,20 +1,13 @@
 import { ServerSidePropsUsers } from 'pages/users';
 
-export const UsersPage = ({ users }: ServerSidePropsUsers) => {
-  console.log('🔎 Log ~ users', users);
+import { UserRow } from './components';
 
+export const UsersPage = ({ users }: ServerSidePropsUsers) => {
   return (
     <div>
       <h3>👤 Users</h3>
       {users.map((user) => (
-        <div key={user.id}>
-          {user.name} | {user.username} | {user.role} |{' '}
-          <ul>
-            <li>Cooked {user.batches.length} batches</li>
-            {/* <li>Best purity {Math.max(user.batches.map((batch) => batch.purity as unknown as number))}</li> */}
-            {/* <li>Average purity {user.batches.map()}</li> */}
-          </ul>
-        </div>
+        <UserRow key={user.id} user={user} />
       ))}
     </div>
   );
