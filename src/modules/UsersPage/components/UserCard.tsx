be76e9b7a703eb3c943da09f9c84959362ad9c88
mkosir <kosir.mar@gmail.com>
2022-07-12 13:53:11 +0200

    chore: update api

--- src/modules/UsersPage/components/UserCard.tsx
+++ src/modules/UsersPage/components/UserCard.tsx
@@ -21,32 +21,30 @@ export const UserCard = ({ user }: UserCardProps) => {
   return (
     <Tilt tiltMaxAngleX={8} tiltMaxAngleY={8} style={{ maxWidth: '400px' }}>
       <Link href={`users/${user.id}`} isUnstyled>
-        <a style={{ textDecoration: 'none', color: 'inherit' }}>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              backgroundColor: 'rgba(172, 201, 243, 0.07)',
-              margin: '10px 0',
-              padding: '15px',
-              borderRadius: '5px',
-              boxShadow: '0 4px 8px 0 rgba(13, 99, 220, 0.13)',
-            }}
-          >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            backgroundColor: 'rgba(172, 201, 243, 0.07)',
+            margin: '10px 0',
+            padding: '15px',
+            borderRadius: '5px',
+            boxShadow: '0 4px 8px 0 rgba(13, 99, 220, 0.13)',
+          }}
+        >
+          <div>
             <div>
-              <div>
-                {user.name} - {user.username}
-              </div>
-              <div>Current role: {user.role.toLowerCase()}</div>
-              <div>
-                <li>Cooked {user.batches.length} batches</li>
-                <li>Best purity {purityBest}</li>
-                <li>Average purity {purityAverage}</li>
-              </div>
+              {user.name} - {user.username}
             </div>
-            <img src={user.imageUrl ?? '/no-user.jpg'} alt="user" width={85} style={{ borderRadius: '4px' }} />
+            <div>Current role: {user.role.toLowerCase()}</div>
+            <div>
+              <li>Cooked {user.batches.length} batches</li>
+              <li>Best purity {purityBest}</li>
+              <li>Average purity {purityAverage}</li>
+            </div>
           </div>
-        </a>
+          <img src={user.imageUrl ?? '/no-user.jpg'} alt="user" width={85} style={{ borderRadius: '4px' }} />
+        </div>
       </Link>
     </Tilt>
   );
