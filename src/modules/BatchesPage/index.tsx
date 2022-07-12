2cfbad0bea4e26b0583fd4b2a001544a6e6eee0a
mkosir <kosir.mar@gmail.com>
2022-07-12 13:55:11 +0200

    chore: update api

--- src/modules/BatchesPage/index.tsx
+++ src/modules/BatchesPage/index.tsx
@@ -1,15 +1,15 @@
 import { ServerSidePropsBatches } from 'pages/batches';
 
-import { BatchRow } from './components';
+import { BatchRow } from './components/BatchRow';
 
 export const BatchesPage = ({ batches }: ServerSidePropsBatches) => {
   return (
-    <div>
+    <>
       <h3>💊 Batches</h3>
       <div style={{ paddingBottom: '10px', fontStyle: 'italic' }}>Total No. of batches: {batches.length}</div>
       {batches.map((batch) => (
         <BatchRow key={batch.id} batch={batch} />
       ))}
-    </div>
+    </>
   );
 };
--- src/modules/UsersPage/index.tsx
+++ src/modules/UsersPage/index.tsx
@@ -4,12 +4,12 @@ import { UserCard } from './components/UserCard';
 
 export const UsersPage = ({ users }: ServerSidePropsUsers) => {
   return (
-    <div>
+    <>
       <h3>👤 Users</h3>
       <div style={{ paddingBottom: '10px', fontStyle: 'italic' }}>Total No. of users: {users.length}</div>
       {users.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
-    </div>
+    </>
   );
 };
