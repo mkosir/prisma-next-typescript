68d61284863daf082e5bcdf692e555f14fbaee0e
mkosir <kosir.mar@gmail.com>
2022-07-12 09:06:38 +0200

    chore: update api

--- src/modules/UsersPage/components/index.tsx
+++ src/modules/UsersPage/components/index.tsx
@@ -1,4 +1,5 @@
 import { Prisma } from '@prisma/client';
+import Tilt from 'react-parallax-tilt';
 
 import { ServerSidePropsUsers } from 'pages/users';
 
@@ -15,13 +16,27 @@ export const UserCard = ({ user }: UserRowProps) => {
   const purityAverage = purities.length ? `${Prisma.Decimal.sum(...purities).toNumber()}%` : '-';
 
   return (
-    <div>
-      {user.name} | {user.username} | {user.role} | Stats:
-      <ul>
-        <li>Cooked {user.batches.length} batches</li>
-        <li>Best purity {purityBest}</li>
-        <li>Average purity {purityAverage}</li>
-      </ul>
-    </div>
+    <Tilt tiltMaxAngleX={8} tiltMaxAngleY={8} style={{ width: '400px' }}>
+      <img src="https://static.wikia.nocookie.net/breakingbad/images/e/e7/BB-S5B-Walt-590.jpg" alt="user" width={85} />
+      <div
+        style={{
+          backgroundColor: 'rgba(172, 201, 243, 0.1)',
+          margin: '10px 0',
+          padding: '15px',
+          borderRadius: '5px',
+          boxShadow: '0 4px 8px 0 rgba(13, 99, 220, 0.2)',
+        }}
+      >
+        <div>
+          {user.name} - {user.username}
+        </div>
+        <div>Current role: {user.role.toLowerCase()}</div>
+        <div>
+          <li>Cooked {user.batches.length} batches</li>
+          <li>Best purity {purityBest}</li>
+          <li>Average purity {purityAverage}</li>
+        </div>
+      </div>
+    </Tilt>
   );
 };
