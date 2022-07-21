e1c3ae32b1dbf62bdaefffb855bf9de8440ccc5f
mkosir <kosir.mar@gmail.com>
2022-07-21 22:22:08 +0200

    chore: update api

--- src/common/consts/Path.ts
+++ src/common/consts/Path.ts
@@ -1,7 +1,7 @@
 export const Path = {
   HOME: '/',
   BATCHES: '/batches',
-  USERS: '/users',
+  USERS: '/useXrs',
   USERS_ADD: '/users/add',
   USERS_DETAILS: (username: string) => `/users/${username}`,
 } as const;
