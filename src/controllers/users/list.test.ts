26c3d2f9498d695dfabcfaa00e6f0104f78a40a9
mkosir <kosir.mar@gmail.com>
2022-07-27 22:11:38 +0200

    chore: update api

---
+++ src/controllers/users/list.test.ts
@@ -0,0 +1,33 @@
+import { User } from '@prisma/client';
+
+import { pathsApiV1 } from 'common/consts/pathsApi';
+import { testClient } from 'common/test/testClient';
+import prisma from 'prisma/prismaClient';
+import { users } from 'prisma/seed/users';
+
+describe('Controllers', () => {
+  describe('Users - List', () => {
+    beforeAll(async () => {
+      await prisma.user.createMany({
+        data: users,
+      });
+    });
+
+    afterAll(async () => {
+      const deleteUsers = prisma.user.deleteMany();
+
+      await prisma.$transaction([deleteUsers]);
+
+      await prisma.$disconnect();
+    });
+
+    it('should list users when endpoint is called', async () => {
+      const { status, data } = await testClient<ReadonlyArray<User>>(pathsApiV1.USERS);
+
+      expect(status).toEqual(200);
+      expect(data[0].email).toEqual(users[0].email);
+      expect(data[0].role).toEqual(users[0].role);
+      expect(data.length).toHaveLength(users.length);
+    });
+  });
+});
