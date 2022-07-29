c462af363fe8fd2fcbb696e2147bf89e3254f69c
mkosir <kosir.mar@gmail.com>
2022-07-29 15:45:48 +0200

    chore: update api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>Prisma/Next.js/TypeScript Boilerplate</title>
+        <title>Prisma / Next.js / TypeScript Boilerplate</title>
         <meta name="description" content="💊 Minimal RESTful API boilerplate with Prisma, Next.js and TypeScript" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={{ textAlign: 'center', margin: '15px 0 40px' }}>
         <img src="/heisenberg.png" alt="heisenberg" width={85} />
-        <div style={{ fontSize: '22px', fontWeight: 'bold' }}>
-          Minimal boilerplate with Prisma, Next.js and TypeScript
-        </div>
+        <div style={{ fontSize: '22px', fontWeight: 'bold' }}>Minimal boilerplate - Prisma / Next.js / TypeScript</div>
       </div>
       <Breadcrumbs />
       <Component {...pageProps} />
