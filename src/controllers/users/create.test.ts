1194a4fef068962b0016260fa6486cfb4e5ac3cf
mkosir <kosir.mar@gmail.com>
2022-07-29 10:47:15 +0200

    chore: update api

--- src/controllers/users/create.test.ts
+++ src/controllers/users/create.test.ts
@@ -2,6 +2,7 @@ import { Prisma } from '@prisma/client';
 
 import { pathsApiV1 } from 'common/consts/pathsApi';
 import { client } from 'common/test/client';
+import { ResponseError } from 'common/types/apiV1';
 import prisma from 'prisma/prismaClient';
 
 describe('Controllers', () => {
@@ -29,6 +30,33 @@ describe('Controllers', () => {
 
       expect(status).toEqual(201);
       expect(data).toEqual(null);
+
+      await prisma.user.delete({ where: { email: userMock.email } });
+    });
+
+    it('should throw an error when user already exists', async () => {
+      await client.post<Prisma.UserCreateInput, null>(
+        pathsApiV1.USERS,
+        { ...userMock },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      const { status, data } = await client.post<Prisma.UserCreateInput, ResponseError>(
+        pathsApiV1.USERS,
+        { ...userMock },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      expect(status).toEqual(400);
+      expect(data).toEqual<ResponseError>({ message: 'An error occurred while creating the user' });
 
       await prisma.user.delete({ where: { email: userMock.email } });
     });
