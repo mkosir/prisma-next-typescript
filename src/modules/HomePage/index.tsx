64180b0f18431555716ce23d4e394fd192af166c
mkosir <kosir.mar@gmail.com>
2022-08-03 14:40:05 +0200

    chore: update api

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Link } from 'common/components';
 import { paths } from 'common/consts/paths';
 import { pathsApiV1 } from 'common/consts/pathsApiV1';
@@ -5,24 +7,37 @@ import { ResponseError } from 'common/types/apiV1';
 import { client } from 'common/utils/client';
 
 export const HomePage = () => {
+  const [isSeeding, setIsSeeding] = useState<boolean>(false);
+
   const seedDb = async () => {
+    setIsSeeding(true);
     await client.post<null, null | ResponseError>(pathsApiV1.DATABASES, null);
+    setIsSeeding(false);
   };
 
   return (
     <div>
-      <button
+      <div
         style={{
-          fontSize: '12px',
-          backgroundColor: '#3d9cc23b',
-          padding: '2px 8px',
-          borderRadius: '3px',
-          marginTop: '10px',
+          display: 'flex',
+          alignItems: 'center',
         }}
-        onClick={seedDb}
       >
-        Seed DB
-      </button>
+        <button
+          style={{
+            fontSize: '12px',
+            backgroundColor: '#3d9cc23b',
+            padding: '2px 8px',
+            borderRadius: '3px',
+            marginTop: '10px',
+          }}
+          disabled={isSeeding}
+          onClick={seedDb}
+        >
+          Seed DB
+        </button>
+        {isSeeding && <div>Loading...</div>}
+      </div>
       <h3>Models:</h3>
       <ul>
         <Link href={paths.USERS}>
