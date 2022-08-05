31ebb9f9b780cdffe1e7f22551d8f6aed5c83ef2
mkosir <kosir.mar@gmail.com>
2022-08-05 15:47:06 +0200

    chore: update api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,33 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
+
+import { Breadcrumbs, Progress } from 'common/components';
 
 import 'common/styles/globals.css';
-import { Breadcrumbs } from 'common/components';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const [isPageLoading, setIsPageLoading] = useState(false);
+
+  useEffect(() => {
+    const routeEventStart = () => {
+      setIsPageLoading(true);
+    };
+    const routeEventEnd = () => {
+      setIsPageLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', routeEventStart);
+    Router.events.on('routeChangeComplete', routeEventEnd);
+    Router.events.on('routeChangeError', routeEventEnd);
+    return () => {
+      Router.events.off('routeChangeStart', routeEventStart);
+      Router.events.off('routeChangeComplete', routeEventEnd);
+      Router.events.off('routeChangeError', routeEventEnd);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -17,7 +40,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <div style={{ fontSize: '22px', fontWeight: 'bold' }}>Minimal boilerplate - Prisma / Next.js / TypeScript</div>
       </div>
       <Breadcrumbs />
-      <Component {...pageProps} />
+      {isPageLoading ? <Progress /> : <Component {...pageProps} />}
     </>
   );
 };
