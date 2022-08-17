bd1a6a3e433214e15377a827f8e954a403ab76d5
mkosir <kosir.mar@gmail.com>
2022-08-17 10:18:33 +0200

    chore: update api

--- src/prisma/seed/index.ts
+++ src/prisma/seed/index.ts
@@ -22,6 +22,8 @@ main()
     console.error(e);
     process.exit(1);
   })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+  .finally(
+    void (async () => {
+      await prisma.$disconnect();
+    }),
+  );
